3002095521dc6bfa5c45fd680e3948fd5cfa80fb
Aishwarya-Birajdar <aishwaryadayanand19@gmail.com>
2025-01-13 07:51:37 -0800

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,40 @@
+package Anudip;
+
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+  /* Write a Java program to take Employee id,name,address & salary from
+      user and display on to screen.
+   */
+
+		Scanner sc = new Scanner(System.in);
+		
+		// Employee id, name, address and salary enter through user.
+		System.out.println("Enter Employee Id : ");
+		int id = sc.nextInt();   // id
+		
+		System.out.println("Enter Employee Name : ");
+		String name = sc.next(); //name
+		
+		System.out.println("Enter Employee Address : ");
+		String address = sc.next(); // address
+		
+		System.out.println("Enter Employee Salary : ");
+		float salary = sc.nextFloat(); // salary
+		
+		// Display id, name, address, salary
+		System.out.println("1. Id : "+id);
+		System.out.println("2. Name : "+name);
+		System.out.println("3. Address : "+address);
+		System.out.println("4. salary : "+salary);
+		
+		/* Output :
+		      
+		 */
+		
+    }
+
+}
