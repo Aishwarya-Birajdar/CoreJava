44779a3f78f543072bc7e33350236a1c496a0f34
Aishwarya-Birajdar <aishwaryadayanand19@gmail.com>
2025-01-14 06:21:12 -0800

    Add files via upload

---
+++ CheckEligiblelity.java
@@ -0,0 +1,31 @@
+package Anudip;
+
+import java.util.Scanner;
+
+public class CheckEligiblelity {
+
+	public static void main(String[] args) {
+		
+	// Q. 2. Write java program to check  candidate eligible for voting or not.
+		
+		// User Input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Age : "); 
+		int age = sc.nextInt();   // age input from user
+		
+		// Logic
+		if (age >= 18) {
+			System.out.println("Eligible For Vote");
+		} else {
+			System.out.println("Not Eligible for voting");
+		}
+		
+		/* Output : 
+		      Enter Age : 
+              34
+              Eligible For Vote
+		 */
+
+	}
+
+}
